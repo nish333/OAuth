0fe9bf643330fa155268774ba97fe3f3da1b64f2
NISHANTH S <110479434+nish333@users.noreply.github.com>
2024-04-30 18:46:12 +0530

    Add files via upload

---
+++ OAuth2.0/src/main/java/com/OAuth/demo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.OAuth.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class HomeController 
+{
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() 
+    {
+        return ResponseEntity.ok("Hello OAuth2");
+    }
+}
